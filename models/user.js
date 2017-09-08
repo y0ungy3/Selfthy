6b64780ad5024fa02c8fa102aa66a196dbebfc32
Dung Mai <dungqmai@yahoo.com>
2017-09-07 22:47:45 -0400

    styled profile and edit profile a little

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ var schema = new Schema({
     username: {type: String, required: true, unique: true, minlength: 6, maxlength: 100},
     password: {type: String, required: true, minlength: 6},
     views: {type: Number},
-    description: {type: String, maxLength: 400},
+    description: {type: String, maxLength: 800},
     createdAt: {type: Date, default: Date.now}
 });
 
